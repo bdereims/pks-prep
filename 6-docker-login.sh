c6184bc1a8dee9d1f50273c8a3052bb706c6b6ca
Administrator <admin@example.com>
2018-04-04 19:22:19 +0200

    add notary settings

--- 6-docker-login.sh
+++ 6-docker-login.sh
@@ -2,5 +2,14 @@
 
 . ./env
 
-HARBOR=harbor.pks-reg.gv
-docker login ${HARBOR} -u ${ADMIN} -p ${ADMIN_PASSWORD}
+### Copy from Harbor VM those files /data/cert/server.*
+### Rename server.crt to server.cert
+
+CERTS=/etc/docker/certs.d/${REG_FQDN}
+sudo mkdir -p ${CERTS} 
+sudo cp ca.crt server.* ${CERTS}/.
+mkdir -p ~/.docker/tls/${REG_FQDN}\:4443
+cp ${CERTS}/* ~/.docker/tls/${REG_FQDN}\:4443/.
+
+sudo systemctl restart docker
+docker login ${REG_FQDN} -u ${ADMIN} -p ${ADMIN_PASSWORD}
