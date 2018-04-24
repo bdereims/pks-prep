31a392778e0b6a89565157be4b1be53bfd9e952a
Administrator <admin@example.com>
2018-04-24 13:24:43 +0200

    add product upload and bosh env automated creation

--- 3-pks_create_cluster.sh
+++ 3-pks_create_cluster.sh
@@ -3,3 +3,5 @@
 . ./env
 
 pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME} --plan small --num-nodes 3 
+
+echo "Wait cluster creation and do: pks get-credentials [cluster-name]"
--- 5-bosh_env.sh
+++ 5-bosh_env.sh
@@ -4,5 +4,12 @@
 
 # USER : director
 # PASSWD : https://OPS-MANAGER-FQDN/api/v0/deployed/director/credentials/director_credentials
+rm -fr ~/.bosh
+
+om -t https://${OPSMANAGER} -u "admin" -p "VMware1!" -k curl -p /api/v0/certificate_authorities -s | jq -r '.certificate_authorities | select(map(.active == true))[0] | .cert_pem' > ca.crt 
+
 bosh alias-env pks -e ${DIRECTOR} --ca-cert ca.crt
-bosh -e pks log-in
+
+PASSWD=$( om -t https://${OPSMANAGER} -u "admin" -p "VMware1!" -k curl -p /api/v0/deployed/director/credentials/director_credentials -s | jq '.[] | .value.password' | sed -e "s/\"//g" )
+
+echo -e "director\n${PASSWD}" | bosh -e pks log-in
