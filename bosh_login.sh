9189004ffca2e649cc3605c9c3ee120c7df7cb76
Brice Dereims <bdereims@gmail.com>
2018-06-04 21:08:32 +0200

    add bosh login script

---
+++ bosh_login.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. ./env
+
+# USER : director
+# PASSWD : https://OPS-MANAGER-FQDN/api/v0/deployed/director/credentials/director_credentials
+
+PASSWD=$( om -t https://${OPSMANAGER} -u "admin" -p "VMware1!" -k curl -p /api/v0/deployed/director/credentials/director_credentials -s | jq '.[] | .value.password' | sed -e "s/\"//g" )
+
+echo -e "director\n${PASSWD}" | bosh -e pks log-in
