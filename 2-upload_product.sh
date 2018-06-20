148ab58f36b09ff711ed3f0bf8478d2345527dfc
Brice Dereims <bdereims@gmail.com>
2018-06-20 12:14:15 +0200

    -

--- 2-upload_product.sh
+++ 2-upload_product.sh
@@ -3,11 +3,11 @@
 . ./env
 
 function upload_product {
-	om -t https://${OPSMANAGER} -k -u "admin" -p "${ADMIN_PASSWORD}" upload-product --product ${1} 
+	om -t https://${OPSMANAGER} -k -u "${ADMIN}" -p "${ADMIN_PASSWORD}" upload-product --product ${1} 
 }
 
 function upload_stemcell {
-	om -t https://${OPSMANAGER} -k -u "admin" -p "${ADMIN_PASSWORD}" upload-stemcell --stemcell ${1} 
+	om -t https://${OPSMANAGER} -k -u "${ADMIN}" -p "${ADMIN_PASSWORD}" upload-stemcell --stemcell ${1} 
 }
 
 #for FILE in $( ls ${BITS}/PKS/*.pivotal ); do
@@ -15,5 +15,5 @@ function upload_stemcell {
 #done
 
 upload_product ${BITS}/PKS/pivotal-container-service-1.0.4-build.5.pivotal 
-upload_product ${BITS}/PKS/harbor-container-registry-1.4.1-build.1.pivotal
+upload_product ${BITS}/PKS/harbor-container-registry-1.4.2-build.14.pivotal
 upload_stemcell ${BITS}/PKS/bosh-stemcell-3468.42-vsphere-esxi-ubuntu-trusty-go_agent.tgz
