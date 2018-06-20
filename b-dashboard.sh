9197c47d23095a211a91b3d0d6a9dc2d583fccef
Brice Dereims <bdereims@gmail.com>
2018-06-20 15:37:49 +0200

    expose dashboard trough LB

--- b-dashboard.sh
+++ b-dashboard.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+kubectl -n kube-system delete svc/kubernetes-dashboard
+
 cat <<EOF | kubectl create -f -
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: ClusterRoleBinding
@@ -15,4 +17,24 @@ subjects:
 - kind: ServiceAccount
   name: kubernetes-dashboard
   namespace: kube-system
+EOF
+
+cat <<EOF | kubectl create -f -
+apiVersion: v1
+kind: Service
+metadata:
+  annotations:
+  labels:
+    k8s-app: kubernetes-dashboard
+  name: kubernetes-dashboard
+  namespace: kube-system
+spec:
+  ports:
+  - name: https
+    targetPort: 8443
+    port: 8443
+  selector:
+    k8s-app: kubernetes-dashboard
+  sessionAffinity: None
+  type: LoadBalancer
 EOF
