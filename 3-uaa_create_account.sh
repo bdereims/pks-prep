67cbf64916d8ae4fc84a0b978ecebe24feca0854
Brice Dereims <bdereims@gmail.com>
2018-06-26 19:04:10 +0200

    -

--- 3-uaa_create_account.sh
+++ 3-uaa_create_account.sh
@@ -5,4 +5,4 @@
 uaac target https://${UAA_FQDN}:8443 --skip-ssl-validation
 uaac token client get admin -s ${UAA_ADMIN_SECRET}
 uaac user add ${ADMIN} --emails ${ADMIN}@${PKS_FQDN} -p ${PASSWORD} 
-uaac member add pks.clusters.admin vmware
+uaac member add pks.clusters.admin ${ADMIN} 
