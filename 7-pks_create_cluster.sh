4061232973e3598f0d2f84faf1ad86981faa15e4
Brice Dereims <bdereims@gmail.com>
2018-07-08 22:43:35 +0200

    -

--- 7-pks_create_cluster.sh
+++ 7-pks_create_cluster.sh
@@ -2,6 +2,6 @@
 
 . ./env
 
-pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME} --plan small --num-nodes 4
+pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME} --plan small --num-nodes 3
 
 echo "Wait cluster creation and do: pks get-credentials [cluster-name]"
