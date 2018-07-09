fcb9635e1ab445e3b659016417f11130d6418dda
Brice Dereims <bdereims@gmail.com>
2018-07-09 15:40:30 +0200

    improve generation of NSX cert

--- nsx-t/4-nsx-cert.sh
+++ nsx-t/4-nsx-cert.sh
@@ -7,7 +7,41 @@
 
 . ../env
 
+NETWORK_MANAGER_USERNAME="admin"
+NETWORK_MANAGER_PASSWORD=$NSX_COMMON_PASSWORD
+
 NODE_ID=$(cat /proc/sys/kernel/random/uuid)
+
+# This function returns id from rest response
+function get_response_id() {
+  echo ${1} | jq '.results[] | .id ' | tr -d '""' 
+}
+
+# This function imports certificat in NSX Manager
+function import_certificat() {
+  CERT=$(awk '{printf "%s\\n", $0}' nsx.crt)
+  KEY=$(awk '{printf "%s\\n", $0}' nsx.key)
+  local certificat_json="{ \
+    \"pem_encoded\":\"${CERT}\", \
+    \"private_key\":\"${KEY}\" \
+  }"
+
+  get_rest_response "api/v1/trust-management/certificates?action=import" "$certificat_json"
+}
+
+# This function returns REST response. Currently based on payload it decides whether to use
+# GET or POST
+function get_rest_response() {
+  local api=${1}
+  local payload=${2}
+
+  if [[ ! -n "$payload" ]]; then
+    curl -sS -k -u $NETWORK_MANAGER_USERNAME:$NETWORK_MANAGER_PASSWORD https://$NSX_MANAGER_IP/$api
+  else
+    curl -sS -H "Content-Type: application/json" -k -u $NETWORK_MANAGER_USERNAME:$NETWORK_MANAGER_PASSWORD -X POST -d "$payload" https://$NSX_MANAGER_IP/$api
+  fi
+
+}
 
 cat nsx-cert.cnf | sed -e "s/###NSX_CN###/${NSX_COMMON_DOMAIN}/" > /tmp/nsx-cert.cnf
 
@@ -21,11 +55,12 @@ openssl req -newkey rsa:2048 -x509 -nodes \
 -reqexts SAN -extensions SAN -config <(cat /tmp/nsx-cert.cnf \
  <(printf "[SAN]\nsubjectAltName=DNS:${NSX_COMMON_DOMAIN}")) -sha256 -days 3650
 
-exit 0
-
 # curl --insecure -u admin:'VMware1!' -X GET https://172.18.13.4/api/v1/trust-management/certificates | jq '.results[] | select(.display_name=="NSX-T Certificat") | .id'
 # curl --insecure -u admin:'VMware1!' -X POST 'https://172.18.13.4/api/v1/node/services/http?action=apply_certificate&certificate_id=63123c9d-8d61-4807-b803-4667adc10425'
-#exit 0 
+
+response=$(import_certificat)
+certificat_id=$(get_response_id "$response")
+curl --insecure -u admin:'VMware1!' -X POST "https://$NSX_MANAGER_IP/api/v1/node/services/http?action=apply_certificate&certificate_id=${certificat_id}"
 
 openssl req \
 -newkey rsa:2048 \
@@ -74,6 +109,7 @@ curl -k -X POST \
 -H 'content-type: application/json' \
 -d "$pi_request"
 
+sleep 5 
 printf "\n\nTest:\n"
 curl -k -X GET \
 "https://${NSX_MANAGER_IP}/api/v1/trust-management/principal-identities" \
