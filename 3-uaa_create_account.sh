fcacf6c8fdabf78f7200a6b4f8cb1b9497ffa87b
Brice Dereims <bdereims@gmail.com>
2018-07-10 07:51:57 +0200

    auto retrieve of UAA Admin passowrd

--- 3-uaa_create_account.sh
+++ 3-uaa_create_account.sh
@@ -2,8 +2,11 @@
 
 . ./env
 
+GUID=$(om -t https://${OPSMANAGER} -u "${ADMIN}" -p "${PASSWORD}" -k curl -p https://opsmanager.cpod-lab.shwrfr.mooo.com/api/v0/deployed/products -s | jq '.[] | select(.installation_name | contains("pivotal-container-service"))  | .guid' | tr -d '""')
+ADMIN_SECRET=$(om -t https://${OPSMANAGER} -u "${ADMIN}" -p "${PASSWORD}" -k curl -p https://opsmanager.cpod-lab.shwrfr.mooo.com/api/v0/deployed/products/${GUID}/credentials/.properties.pks_uaa_management_admin_client -s | jq '.credential.value.secret' | tr -d '""')
+
 uaac target https://${UAA_FQDN}:8443 --skip-ssl-validation
-uaac token client get admin -s ${UAA_ADMIN_SECRET}
+uaac token client get admin -s "${ADMIN_SECRET}"
 uaac user delete ${ADMIN}
 uaac user add ${ADMIN} --emails ${ADMIN}@${PKS_FQDN} -p ${PASSWORD} 
 uaac member add pks.clusters.admin ${ADMIN} 
---
+++ c-cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#bdereims@vmware.com
+
+rm -fr ~/.kube ~/.bosh ~/.pks ~/.helm
+rm ca.crt nsx-t/nsx.* nsx-t/pks-nsx-t-superuser.* nsx-t/ovftool.log
+rn -fr /tmp/*
