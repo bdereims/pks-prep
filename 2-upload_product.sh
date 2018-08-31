12ebe69d29dc99080ef60e4658c2685e095a7ddf
Brice Dereims <vincent.meoc@gmail.com>
2018-08-31 07:43:05 +0200

    improvements

--- 2-upload_product.sh
+++ 2-upload_product.sh
@@ -10,12 +10,8 @@ function upload_stemcell {
 	om -t https://${OPSMANAGER} -k -u "${ADMIN}" -p "${PASSWORD}" upload-stemcell --stemcell ${1} 
 }
 
-#for FILE in $( ls ${BITS}/PKS/*.pivotal ); do
-#	upload_product ${FILE}
-#done
-
+rm -fr /tmp/*
 upload_product ${BITS}/PKS/pivotal-container-service-1.1.4-build.5.pivotal
 upload_product ${BITS}/PKS/harbor-container-registry-1.5.2-build.8.pivotal
 upload_stemcell ${BITS}/PKS/bosh-stemcell-3586.27-vsphere-esxi-ubuntu-trusty-go_agent.tgz
 upload_stemcell ${BITS}/PKS/bosh-stemcell-3468.42-vsphere-esxi-ubuntu-trusty-go_agent.tgz
-
