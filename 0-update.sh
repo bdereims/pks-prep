c1ddd326122a31b8469a36f1e78e6857d827fab8
Eric De Witte <edewitte@edewitte-a01.vmware.com>
2018-10-06 14:31:39 +0200

    updated releases for bosh, helm, om client + added pivnet install and download pks cli

--- 0-update.sh
+++ 0-update.sh
@@ -3,9 +3,12 @@
 #Only tested on Ubuntu 16.04/18.04 LTS
 
 BINDIR=/usr/local/bin
-BOSHRELEASE=3.0.1
-HELMRELEASE=2.9.0
-OMRELEASE=0.37.0
+BOSHRELEASE=5.3.1
+HELMRELEASE=2.11.0
+OMRELEASE=0.41.0
+PIVNETRELEASE=0.0.54
+APIREFRESHTOKEN=<insert-refresh-token-here>
+PKSRELEASE=1.1.5
 
 sudo apt-get update ; sudo apt-get upgrade
 sudo apt-get install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 sshpass jq dnsmasq iperf3
@@ -39,7 +42,19 @@ sudo cp linux-amd64/helm ${BINDIR}/helm
 rm -fr linux-amd64
 rm helm-v${HELMRELEASE}-linux-amd64.tar.gz
 
+# pivnet
+curl -LO https://github.com/pivotal-cf/pivnet-cli/releases/download/v${PIVNETRELEASE}/pivnet-linux-amd64-${PIVNETRELEASE}
+
+sudo chown root pivnet-linux-amd64-${PIVNETRELEASE}
+sudo chmod ugo+x pivnet-linux-amd64-${PIVNETRELEASE}
+sudo mv pivnet-linux-amd64-${PIVNETRELEASE} ${BINDIR}/pivnet
+
 # pks
+pivnet login --api-token=$APIREFRESHTOKEN
+PKSFileID=`pivnet pfs -p pivotal-container-service -r $PKSRELEASE | grep 'PKS CLI - Linux' | awk '{ print $2}'`
+pivnet download-product-files -p pivotal-container-service -r $PKSRELEASE -i $PKSFileID
+
+mv pks-linux-amd64* pks 
 sudo chown root:root pks
 sudo chmod +x pks
 sudo cp pks ${BINDIR}/pks
