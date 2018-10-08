a27e48d42f5fd66c427ae2b3a152dc074a2e2488
Eric De Witte <edewitte@vmware.com>
2018-10-08 09:23:32 +0200

    fix download_vmware.sh

--- download_vmware.sh
+++ download_vmware.sh
@@ -19,23 +19,23 @@ fi
 
 # OVFTOOL
 vmw-cli index $OVFTOOLPG
-ovfFileName=`vmw-cli json productGroup:$OVFTOOLPG,fileName:lin.X86 | jq -r '.fileName'`
+export ovfFileName=`vmw-cli json productGroup:$OVFTOOLPG,fileName:lin.X86 | jq -r '.fileName'`
 vmw-cli get $ovfFileName
 mv $ovfFileName $BITSDIR
 
 # NSX-T
 vmw-cli index $NSXTPG
 # get manager
-nsxMgrFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:unified | jq -r '.fileName'`
+export nsxMgrFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:unified | jq -r '.fileName'`
 vmw-cli get $nsxMgrFileName
 mv $nsxMgrFileName $BITSDIR
 
 #get controller
-nsxCtrlFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:controller | jq -r '.fileName'`
+export nsxCtrlFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:controller | jq -r '.fileName'`
 vmw-cli get $nsxCtrlFileName
 mv $nsxCtrlFileName $BITSDIR
 
 #get edge
-nsxEdgeFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:edge | jq -r '.fileName'`
+export nsxEdgeFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:edge | jq -r '.fileName'`
 vmw-cli get $nsxEdgeFileName
 mv $nsxEdgeFileName $BITSDIR
