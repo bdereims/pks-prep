d3596c5e7c8ad95dace7a7222bb581c2e7fc31f8
Eric De Witte <edewitte@vmware.com>
2018-10-13 11:28:01 +0200

    added xenial stemcells param and download + filenames env file placeholder

--- download_pivotal.sh
+++ download_pivotal.sh
@@ -3,13 +3,11 @@
 # get download env
 source define_download_version_env
 
-
 if [ $APIREFRESHTOKEN = "<insert-refresh-token-here>" ]
 then
     echo "Update APIREFRESHTOKEN value in set_env before running it"
     exit 1
 fi
-
 
 # pks cli
 pivnet login --api-token=$APIREFRESHTOKEN
@@ -26,21 +24,21 @@ OpsmanFileId=`pivnet pfs -p ops-manager -r $OPSMANRELEASE | grep 'pcf-vsphere' |
 pivnet accept-eula -p ops-manager -r $OPSMANRELEASE
 pivnet download-product-files -p ops-manager -r $OPSMANRELEASE -i $OpsmanFileId
 export OpsmanFileName=`ls pcf-vsphere-*`
-mv $OpsmanFileName $BITSDIR
+mv $OpsmanFileName $BITSDIR$PKSFOLDER
 
 # pks tile
 PKSFileID=`pivnet pfs -p pivotal-container-service -r $PKSRELEASE | grep pivotal-container-service-$PKSRELEASE | awk '{ print $2 }'`
 pivnet accept-eula -p pivotal-container-service -r $PKSRELEASE
 pivnet download-product-files -p pivotal-container-service -r $PKSRELEASE -i $PKSFileID
 export PKSFileName=`ls pivotal-container-service-*`
-mv $PKSFileName $BITSDIR
+mv $PKSFileName $BITSDIR$PKSFOLDER
 
 #pks stemcell
 StemcellFileId=`pivnet pfs -p stemcells -r $STEMCELLRELEASE | grep 'vsphere' | awk '{ print $2 }'`
 pivnet accept-eula -p stemcells -r $STEMCELLRELEASE
 pivnet download-product-files -p stemcells -r $STEMCELLRELEASE -i $StemcellFileId
 export StemcellFileName=`ls bosh-stemcell-*`
-mv $StemcellFileName $BITSDIR
+mv $StemcellFileName $BITSDIR$PKSFOLDER
 
 #Harbor Tile
 
@@ -48,4 +46,12 @@ HarborFileId=`pivnet pfs -p harbor-container-registry -r $HARBORRELEASE --format
 pivnet accept-eula -p harbor-container-registry -r $HARBORRELEASE
 pivnet download-product-files -p harbor-container-registry -r $HARBORRELEASE -i $HarborFileId
 export HarborFileName=`ls harbor-container-registry*`
-mv $HarborFileName $BITSDIR
+mv $HarborFileName $BITSDIR$PKSFOLDER
+
+#pks Xenial stemcell
+StemcellXenialFileId=`pivnet pfs -p stemcells-ubuntu-xenial -r $STEMCELLXENIALRELEASE | grep 'vsphere' | awk '{ print $2 }'`
+pivnet accept-eula -p stemcells-ubuntu-xenial -r $STEMCELLXENIALRELEASE
+pivnet download-product-files -p stemcells-ubuntu-xenial -r $STEMCELLXENIALRELEASE -i $StemcellXenialFileId
+export StemcellXenialFileName=`ls bosh-stemcell*xenial*`
+mv $StemcellFileName $BITSDIR$PKSFOLDER
+echo "export StemcellXenialFileName="$BITSDIR$PKSFOLDER"/"$StemcellXenialFileName
--- download_vmware.sh
+++ download_vmware.sh
@@ -21,21 +21,21 @@ fi
 vmw-cli index $OVFTOOLPG
 export ovfFileName=`vmw-cli json productGroup:$OVFTOOLPG,fileName:lin.X86 | jq -r '.fileName'`
 vmw-cli get $ovfFileName
-mv $ovfFileName $BITSDIR
+mv $ovfFileName $BITSDIR$VMWFOLDER
 
 # NSX-T
 vmw-cli index $NSXTPG
 # get manager
 export nsxMgrFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:unified | jq -r '.fileName'`
 vmw-cli get $nsxMgrFileName
-mv $nsxMgrFileName $BITSDIR
+mv $nsxMgrFileName $BITSDIR$NSXFOLDER
 
 #get controller
 export nsxCtrlFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:controller | jq -r '.fileName'`
 vmw-cli get $nsxCtrlFileName
-mv $nsxCtrlFileName $BITSDIR
+mv $nsxCtrlFileName $BITSDIR$NSXFOLDER
 
 #get edge
 export nsxEdgeFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:edge | jq -r '.fileName'`
 vmw-cli get $nsxEdgeFileName
-mv $nsxEdgeFileName $BITSDIR
+mv $nsxEdgeFileName $BITSDIR$NSXFOLDER
