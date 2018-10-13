e3975e356afeb76ea4668acb0ba2501744e168d5
Eric De Witte <edewitte@vmware.com>
2018-10-13 11:36:22 +0200

    added logging filenames to software_filenames.env

--- download_vmware.sh
+++ download_vmware.sh
@@ -19,23 +19,27 @@ fi
 
 # OVFTOOL
 vmw-cli index $OVFTOOLPG
-export ovfFileName=`vmw-cli json productGroup:$OVFTOOLPG,fileName:lin.X86 | jq -r '.fileName'`
+ovfFileName=`vmw-cli json productGroup:$OVFTOOLPG,fileName:lin.X86 | jq -r '.fileName'`
 vmw-cli get $ovfFileName
-mv $ovfFileName $BITSDIR$VMWFOLDER
+mv $ovfFileName $BITSDIR
+echo "export ovfFileName="$BITSDIR"/"$ovfFileName > software_filenames.env
 
 # NSX-T
 vmw-cli index $NSXTPG
 # get manager
-export nsxMgrFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:unified | jq -r '.fileName'`
+nsxMgrFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:unified | jq -r '.fileName'`
 vmw-cli get $nsxMgrFileName
 mv $nsxMgrFileName $BITSDIR$NSXFOLDER
+echo "export nsxMgrFileName="$BITSDIR$NSXFOLDER"/"$nsxMgrFileName > software_filenames.env
 
 #get controller
-export nsxCtrlFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:controller | jq -r '.fileName'`
+nsxCtrlFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:controller | jq -r '.fileName'`
 vmw-cli get $nsxCtrlFileName
 mv $nsxCtrlFileName $BITSDIR$NSXFOLDER
+echo "export nsxCtrlFileName="$BITSDIR$NSXFOLDER"/"$nsxCtrlFileName > software_filenames.env
 
 #get edge
-export nsxEdgeFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:edge | jq -r '.fileName'`
+nsxEdgeFileName=`vmw-cli json productGroup:$NSXTPG,fileType:ova,fileName:edge | jq -r '.fileName'`
 vmw-cli get $nsxEdgeFileName
 mv $nsxEdgeFileName $BITSDIR$NSXFOLDER
+echo "export nsxEdgeFileName="$BITSDIR$NSXFOLDER"/"$nsxEdgeFileName > software_filenames.env
