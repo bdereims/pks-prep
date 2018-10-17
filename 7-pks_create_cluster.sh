a362ff02f9dec84aaaf15e0818e227d65bf0024e
Brice Dereims <bdereims@gmail.com>
2018-10-17 14:55:27 +0200

    somme mods

--- 7-pks_create_cluster.sh
+++ 7-pks_create_cluster.sh
@@ -3,6 +3,6 @@
 . ./env
 
 pks create-network-profile lb-medium.json
-pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME} --plan default --num-nodes 3 --network-profile lb-profile-medium
+pks create-cluster ${CLUSTER_NAME} --external-hostname ${CLUSTER_NAME} --plan small --num-nodes 3 --network-profile lb-profile-medium
 
 echo "Wait cluster creation and do: pks get-credentials [cluster-name]"
