e7a96473e1eb4255a4cba851b2fbd046ade0420e
Brice Dereims <bdereims@gmail.com>
2019-02-16 23:05:37 +0100

    -

--- k8s/TechSummit/tools/gen.sh
+++ k8s/TechSummit/tools/gen.sh
@@ -6,6 +6,8 @@ DB=web.txt
 
 cp /dev/null $DB 
 
+echo "generating..."
+
 for LINE in {1..1000}
 do
 	NAME=$( ./petname.sh )
@@ -13,5 +15,7 @@ do
 	UUID=$( cat /proc/sys/kernel/random/uuid )
 	printf "${LINE}-${UUID}-${NAME}\t${DATE}\n" >> ${DB} 
 done
+
+echo "loading..."
 
 mysqlimport -h ${HOST} -u root -pVMware1! --local nginx ${DB} 
