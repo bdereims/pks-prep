32d45c6f40dcd02bd3bb4124c83bc6b5244d53f1
Brice Dereims <bdereims@gmail.com>
2019-10-13 13:08:25 +0200

    addons

---
+++ f-skaffold.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+#bdereims@vmware.com
+
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
+chmod +x skaffold
+sudo mv skaffold /usr/local/bin
--- nsx-t/2.5/2-activate_nsx_cluster.sh
+++ nsx-t/2.5/2-activate_nsx_cluster.sh
@@ -18,11 +18,16 @@ function configure_nsx_cluster() {
   echo "Set Service Upgrade On"
   sshpass -p $manager_password ssh -o StrictHostKeyChecking=no root@$manager_ip "/opt/vmware/nsx-cli/bin/scripts/nsxcli -c \"set service install-upgrade enable\""
 
+  echo "Set sending log to vrli"
+  sshpass -p $manager_password ssh -o StrictHostKeyChecking=no root@$manager_ip "/opt/vmware/nsx-cli/bin/scripts/nsxcli -c \"set logging-server ${OVA_VRLI_IP} proto udp level info\""
+
   for EDGE_IP in ${NSX_EDGE_IP[@]}; do
     echo "---"
     echo "Join NSX edge(s) to management plane"
 
     eval sshpass -p $edge_password ssh root@${EDGE_IP} -o StrictHostKeyChecking=no "/opt/vmware/nsx-cli/bin/scripts/nsxcli -c \"join management-plane $manager_ip username admin thumbprint $manager_thumbprint password $manager_password\""
+    
+    eval sshpass -p $edge_password ssh root@${EDGE_IP} -o StrictHostKeyChecking=no "/opt/vmware/nsx-cli/bin/scripts/nsxcli -c \"set logging-server ${OVA_VRLI_IP} proto udp level info\""
   done
 }
 
