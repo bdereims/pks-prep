d10888696524f71e7f02ed966aaf896d69d9807a
Brice Dereims <bdereims@gmail.com>
2019-12-23 14:01:36 +0100

    deploy in aws

--- aws/pivotal-cf-terraforming-aws-73eecd4/modules/ops_manager/keypair.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_key_pair" "ops_manager" {
-  key_name   = "${var.env_name}-ops-manager-key"
-  public_key = "${tls_private_key.ops_manager.public_key_openssh}"
-}
-
-resource "tls_private_key" "ops_manager" {
-  algorithm = "RSA"
-  rsa_bits  = "4096"
-}
